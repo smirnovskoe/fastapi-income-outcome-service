9675135f2dc3aa8bf26f668931df9cfbbd0fdf04
yanix <smirnovskoe@gmail.com>
2021-06-14 15:25:40 +0300

    add database_url to settings class add sqlalchemy: engine, sessions create Operation tables

--- src/workshop/settings.py
+++ src/workshop/settings.py
@@ -4,10 +4,10 @@ from pydantic import BaseSettings
 class Settings(BaseSettings):
     server_host: str = '127.0.0.1'
     port: int = 5000
+    database_url: str = 'sqlite:///./sql_app.sqlite3'
 
 
 settings = Settings(
     _env_file='.env',
     _env_file_encoding='utf-8'
 )
-
