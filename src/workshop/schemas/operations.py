059d663d0abc9ba4db6bfdd1c36d7a3f84e98c4e
yanix <smirnovskoe@gmail.com>
2021-06-15 18:11:21 +0300

    create pydantic schemas

---
+++ src/workshop/schemas/operations.py
@@ -0,0 +1,29 @@
+from datetime import date
+from decimal import Decimal
+from enum import Enum
+from typing import Optional
+
+from pydantic import BaseModel
+
+
+class OperationKind(str, Enum):
+    INCOME = 'income'
+    OUTCOME = 'outcome'
+
+
+class OperationBaseSchema(BaseModel):
+    date: date
+    kind: OperationKind
+    amount: Decimal
+    description: Optional[str]
+
+
+class OperationSchema(OperationBaseSchema):
+    id: int
+
+    class Config:
+        orm_mode = True
+
+
+class OperationCreateSchema(OperationBaseSchema):
+    pass
